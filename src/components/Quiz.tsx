d70ea2030bca2e72293e0f2fb37440ec8d1a429c
atanu98 <chakrabortyatanu238.com>
2025-01-03 09:17:21 +0530

    chnges made

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { resetQuiz, nextQuestion, incrementScore } from "../store/slices/quizSlice";
 import QuestionCard from "./QuestionCard";
